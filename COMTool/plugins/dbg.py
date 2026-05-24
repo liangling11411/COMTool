089bd2e79aea279363400114f5baed35ea5dfa72
liangling11411 <liangling11411@users.noreply.github.com>
2026-05-24 22:50:01 +0800

    Revert "fix(log): bound async writer backlog"
    This reverts commit de7582ad8770373c5396917c619bfeaa387b7c16.

--- COMTool/plugins/dbg.py
+++ COMTool/plugins/dbg.py
@@ -39,9 +39,8 @@ DEFAULT_TEXT_FONT = "Consolas"
 
 
 class AsyncTextFileWriter:
-    def __init__(self, encoding, maxQueueChunks=20000):
+    def __init__(self, encoding):
         self.encoding = encoding
-        self.maxQueueChunks = maxQueueChunks
         self.path = ""
         self.queue = None
         self.thread = None
@@ -52,7 +51,7 @@ class AsyncTextFileWriter:
         self.path = path
         if encoding:
             self.encoding = encoding
-        self.queue = queue.Queue(maxsize=self.maxQueueChunks)
+        self.queue = queue.Queue()
         self.active = True
         self.thread = threading.Thread(target=self.writeProcess)
         self.thread.setDaemon(True)
@@ -60,8 +59,6 @@ class AsyncTextFileWriter:
 
     def write(self, text):
         if not text or not self.active or self.queue is None:
-            return False
-        if self.thread is not None and not self.thread.is_alive():
             return False
         self.queue.put(text)
         return True
@@ -109,17 +106,6 @@ class AsyncTextFileWriter:
                         self.queue.task_done()
                 f.flush()
         except Exception as e:
-            self.active = False
-            if self.queue is not None:
-                while True:
-                    try:
-                        self.queue.get_nowait()
-                    except queue.Empty:
-                        break
-                    try:
-                        self.queue.task_done()
-                    except Exception:
-                        break
             log.e("async log writer failed: {}".format(e))
 
 
